6e4e6a621c14e3d1b73db52129bc97c77c84ffe1
SkylerPIlot <SkylerParthemore@gmail.com>
2017-03-13 18:53:31 -0700

    Initial Commit

---
+++ src/EvoTheLution.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : EvoTheLution.cpp
+// Author      : Skyler Parthemore
+// Version     :
+// Copyright   : Have fun with this 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
